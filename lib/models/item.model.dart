d1d9cd0817d97d9c5d89de878a069a650099ed3d
geovannifranca <geovannidefranca98@gmail.com>
2026-02-19 19:44:32 -0300

    ajuste dos modelos

--- lib/models/item.model.dart
+++ lib/models/item.model.dart
@@ -1,7 +1,18 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:uuid/uuid.dart';
+
 class Item {
-  String name;
-  double price;
-  bool isCompleted;
-  Item({required this.name, required this.price, required this.isCompleted});
+  final String _id;
+  final String _name;
+  final double _value;
+  final bool _isCompleted;
+  Item({required String name, required double value, bool isCompleted = false})
+    : _id = const Uuid().v4(),
+      _name = name, // Repassa para a variável privada
+      _value = value, // Repassa para a variável privada
+      _isCompleted = isCompleted;
+
+  String get id => _id;
+  String get name => _name;
+  double get value => _value;
+  bool get isCompleted => _isCompleted;
 }
--- lib/models/my_list.model.dart
+++ lib/models/my_list.model.dart
@@ -1,5 +1,26 @@
-class MyList {
-  String listName;
+import 'package:app_lista_de_compras/models/item.model.dart';
+import 'package:uuid/uuid.dart';
 
-  MyList({required this.listName});
+class MyList {
+  final String _id;
+  final String _listName;
+  final List<Item> _items;
+
+  MyList({required String listName, required List<Item> items})
+    : _id = const Uuid().v4(),
+      _listName = listName,
+      _items = items;
+
+  String get id => _id;
+  String get listName => _listName;
+  List<Item> get items => _items;
+
+  int get totalItems => _items.length;
+
+  int get completedItems => _items.where((item) => item.isCompleted).length;
+
+  double get progress {
+    if (totalItems == 0) return 0.0;
+    return completedItems / totalItems;
+  }
 }
